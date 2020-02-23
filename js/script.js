170f0df13a54a00cc2caf82a5715e900001fa8d5
Kerson Veloso <kerson213@gmail.com>
2020-02-23 02:43:04 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let display = 0;
+let current = [];
+
+$( document ).ready(function (){
+    $('.display').html(display);
+});
+
+function clr(){
+    current = [];
+    display = 0;
+    $('.display').html(display);
+}
+
+function insert(v){
+    current.push(v);
+    display = current;
+    $('.display').html(display);
+}
+
+function eql(){
+    display = eval(display.join(''));
+    $('.display').html(display);
+}
+
+function per(){
+    display = eval(display.join('')+'/100');
+    $('.display').html(display);
+}
+
+function modify(){
+    display = eval(display.join('')+'*(-1)');
+    $('.display').html(display);
+}
